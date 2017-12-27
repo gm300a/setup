fc4270ebc55114392129c9007430432a507bc2dc
gm300 <gmicro300@gmail.com>
2017-12-27 15:34:24 +0900

    init

---
+++ UBUNTU.init.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install screen kpat tclsh tcsh emacs g++ unclutter git git-gui node
+sudo apt-get install screen kpat tclsh tcsh emacs g++ unclutter git git-gui node
