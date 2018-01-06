3163ed9efcf362ccf1e9b33c7c24df1a2573fa6a
gm300 <gmicro300@gmail.com>
2018-01-06 20:52:38 +0900

    more package and pip

--- UBUNTU.init.sh
+++ UBUNTU.init.sh
@@ -1,4 +1,8 @@
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get install screen kpat tclsh tcsh emacs g++ unclutter git git-gui node
-sudo apt-get install screen kpat tclsh tcsh emacs g++ unclutter git git-gui node
+apt-get update
+apt-get upgrade
+apt-get install screen kpat tclsh tcsh emacs g++ unclutter git git-gui nodejs gawk
+apt-get install python-pip
+#sudo apt-get install screen kpat tclsh tcsh emacs g++ unclutter git git-gui node
+rehash
+pip install --upgrade pip  
+pip install awscli --upgrade --user
